9c87ae1652b0a69e580f71515c621eb93959a90a
xavier pupo <pupoxavier7@gmail.com>
2024-09-28 19:32:23 -0500

    se agrego una aplicacion de tarjeta de credito mediante java

--- src/Compras/Compras.java
+++ src/Compras/Compras.java
@@ -1,4 +1,30 @@
 package Compras;
 
-public class Compras {
+public class Compras implements Comparable<Compras>{
+    private double valor;
+    private String descripcion;
+
+    public Compras(double valor, String descripcion) {
+        this.valor = valor;
+        this.descripcion = descripcion;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Compras: valor =" + valor +
+                ", descripcion ='" + descripcion;
+    }
+
+    @Override
+    public int compareTo(Compras otraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(otraCompra.getValor()));
+    }
 }
--- src/TarjetDeCredito.java
+++ src/TarjetDeCredito.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import Compras.Compras;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TarjetDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compras> listaDeCompras;
+
+    public TarjetDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.listaDeCompras = new ArrayList<>();
+    }
+
+    public boolean lanzarCompra (Compras compras){
+        if (this.saldo > compras.getValor()){
+            this.saldo -= compras.getValor();
+            this.listaDeCompras.add(compras);
+            return true;
+        }
+        return false;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public List<Compras> getListaDeCompras(){
+        return listaDeCompras;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
